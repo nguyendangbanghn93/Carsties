df350e7a8efb9e820cfb696c3d1ef99fc4d68367
bangnd <bangnd@gmail.com>
2024-05-30 09:03:19 +0000

    end of session 6

--- src/AuctionService/Entities/Auction.cs
+++ src/AuctionService/Entities/Auction.cs
@@ -14,14 +14,6 @@ public class Auction
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     public DateTime AuctionEnd { get; set; }
-
     public Status Status { get; set; }
     public Item Item { get; set; }
-
-    // public string Make { get; set; }
-    // public string Model { get; set; }
-    // public int Year { get; set; }
-    // public string Color { get; set; }
-    // public int Mileage { get; set; }
-    // public string ImageUrl { get; set; }
 }
